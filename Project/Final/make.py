951931470bc10d8959049192e74188f66b509cb7
Thoth19 <kpseipp@gmail.com>
2016-06-03 08:31:28 -0700

    Added periperhal code. Ie READme, tests, make etc.

---
+++ Project/Final/make.py
@@ -0,0 +1,12 @@
+# This file takes the place of a makefile because
+# we are going to use tests.py to test the RNGs
+
+import os
+
+print "Compiling the CPU demon"
+os.system("nvcc -std=c++11 -x cu -lcurand main.cu -o main")
+print "Running the code to create 1 000 000 random integers"
+os.system("./a 1000000")
+print "Running tests on those output files"
+os.system("python tests.py")
+print "DONE with demo."
